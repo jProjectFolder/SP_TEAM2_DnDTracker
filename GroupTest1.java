962e755ce521d291a0d7edba3ce581097bda3af6
jProjectFolder <97361552+jProjectFolder@users.noreply.github.com>
2022-02-27 16:28:04 -0600

    Add files via upload
    This is the first commit file for us to practice editing and commiting.

---
+++ GroupTest1.java
@@ -0,0 +1,29 @@
+/*
+Creator:        Jason Haldiman
+Date:           02-27-22
+Purpose:        The purpose of this file is to allow the team to access a file on GitHub and modify it to so it changes the main thread.
+
+This file will have the team members call a printName() 
+method and add there name. 
+
+*/
+
+package SP_TEAM2_DnDTracker;
+public class GroupTest1 {
+    
+
+    public static void main(String[] args){
+        //insert the nameCall() method here with name
+        nameCall("Jason was here!");
+
+
+    }//end main
+
+    
+    
+    //Method that prints passed argument
+    public static void nameCall(String name){
+        System.out.println(name);
+    }//end nameCall
+
+}//end class GroupTest1
